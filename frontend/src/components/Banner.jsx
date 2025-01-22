249452311402ec7af6c70d795ec67de46fbfe123
JohnJung-1017 <jungih1017@gmail.com>
2025-01-22 17:02:07 +0900

    feat: Banner
    상단 공통 배너 생성

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner_container">
+      <div className="banner_logo">Logo</div>
+      <div className="banner_btn">
+        <div className="banner_username">반갑습니다 일형님!</div>
+        <button className="banner_logout">Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;

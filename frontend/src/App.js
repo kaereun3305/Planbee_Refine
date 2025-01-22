b42b00bf302428292822180881d4532f929297d0
JohnJung-1017 <jungih1017@gmail.com>
2025-01-22 17:01:47 +0900

    Update App.js
    게시판을 제외한 페이지 route 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import ToDoList from "./pages/ToDoList";
+import Archive from "./pages/Archive";
+import Callendar from "./pages/Callendar";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        {/* <Route path="/" element={< />} />
-        <Route path="/" element={< />} />
-        <Route path="/" element={< />} />
-        <Route path="/" element={</>} />
-        <Route path="/" element={</>} /> */}
+        <Route path="/" element={<ToDoList />} />
+        {/* 
+            <Route path="/" element={<SignIn />} />
+            <Route path="/todolist" element={<ToDoList />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/callendar" element={<Callendar/>} /> 
+        */}
       </Routes>
     </Router>
   );

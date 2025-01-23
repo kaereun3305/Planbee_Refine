853776073d9e83e6251891358894c0a4d598f17c
JohnJung-1017 <jungih1017@gmail.com>
2025-01-23 22:35:33 +0900

    feat: Calendar.jsx
    페이지 오타 수정 및 화면 비율 조정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import ToDoList from "./pages/ToDoList";
 import Archive from "./pages/Archive";
-import Callendar from "./pages/Callendar";
+import Calendar from "./pages/Calendar";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ToDoList />} />
-        {/* 
-            <Route path="/" element={<SignIn />} />
-            <Route path="/todolist" element={<ToDoList />} />
-            <Route path="/archive" element={<Archive />} />
-            <Route path="/callendar" element={<Callendar/>} /> 
-        */}
+        <Route path="/todolist" element={<ToDoList />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </Router>
   );

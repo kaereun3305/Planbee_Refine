3ab107d53bc4fef023167fe110490576430cb90d
JohnJung-1017 <jungih1017@gmail.com>
2025-01-23 22:35:57 +0900

    feat: SideBar.jsx
    사이드바 구현

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,12 +1,55 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../css/SideBar.css";
 
 const SideBar = () => {
   return (
-    <div className="sidebar_container">
-      <div>1번</div>
-      <div>2번</div>
-    </div>
+    <nav className="sidebar_container">
+      <NavLink
+        className="sidebar_menu"
+        to={"/todolist"}
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "#EFEFEF" : "#FEFCFC",
+          };
+        }}
+      >
+        ToDoList
+      </NavLink>
+      <NavLink
+        className="sidebar_menu"
+        to={"/archive"}
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "#EFEFEF" : "#FEFCFC",
+          };
+        }}
+      >
+        Archive
+      </NavLink>
+      <NavLink
+        className="sidebar_menu"
+        to={"/calendar"}
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "#EFEFEF" : "#FEFCFC",
+          };
+        }}
+      >
+        Calendar
+      </NavLink>
+      <NavLink
+        className="sidebar_menu"
+        to={"/social"}
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "#EFEFEF" : "#FEFCFC",
+          };
+        }}
+      >
+        Social
+      </NavLink>
+    </nav>
   );
 };
 

9a405537a37caeff12ca3f0a3d9ba62b79cc5915
JohnJung-1017 <jungih1017@gmail.com>
2025-01-23 17:35:29 +0900

    Update : SignIn
    로그인 페이지 내부에 회원가입 팝업 연결 및 회원가입 폼 생성

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import PopUp from "../components/PopUp";
 import "../css/SignIn.css";
+import "../css/SignUp.css";
 
-const signIn = () => {
+const SignIn = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupOpen((prev) => !prev);
+  };
+
   return (
     <div className="login_container">
       <div className="login_boxL">Logo</div>
@@ -30,7 +38,83 @@ const signIn = () => {
               <div className="logIn_button">
                 <input className="login_btn" type="submit" value="Login" />
               </div>
-              <button className="login_btn">SignUp</button>
+              <button
+                className="login_btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  togglePopup();
+                }}
+              >
+                SignUp
+              </button>
+              <PopUp isOpen={isPopupOpen} onClose={togglePopup}>
+                <div className="signup_container">
+                  <div className="signup_logo">logo</div>
+                  <form>
+                    <div className="signup_form">
+                      <input
+                        type="text"
+                        id="userId"
+                        name="userId"
+                        placeholder="User ID"
+                      />
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                      />
+                      <input
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                      />
+                      <input
+                        type="text"
+                        id="userName"
+                        name="userName"
+                        placeholder="User Name"
+                      />
+                      <div className="email_certificate">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="Email"
+                        />
+                        <button type="button" className="signup_btn1">
+                          Send Code
+                        </button>
+                      </div>
+                      <div className="email_certificate">
+                        <input
+                          type="text"
+                          id="verificationCode"
+                          name="verificationCode"
+                          placeholder="Verification Code"
+                        />
+                        <button type="button" className="signup_btn2">
+                          Complete
+                        </button>
+                      </div>
+                      <input
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        placeholder="010-1234-5678"
+                      />
+                    </div>
+                    <div className="signup_button">
+                      <input
+                        className="signup_btn"
+                        type="submit"
+                        value="SignUp"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </PopUp>
             </div>
           </form>
         </div>
@@ -39,4 +123,4 @@ const signIn = () => {
   );
 };
 
-export default signIn;
+export default SignIn;

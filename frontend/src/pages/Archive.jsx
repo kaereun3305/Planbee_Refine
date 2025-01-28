c02d8677970b2033f64dcad58fe3c3f80876e729
JohnJung-1017 <jungih1017@gmail.com>
2025-01-28 19:03:52 +0900

    feat: archivePage
    기본 요소 배치

--- frontend/src/pages/Archive.jsx
+++ frontend/src/pages/Archive.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../components/SideBar";
 import Banner from "../components/Banner";
 import "../css/Main.css";
+import "../css/Archive.css";
 
 const Archive = () => {
   return (
@@ -9,7 +10,20 @@ const Archive = () => {
       <Banner />
       <div className="sidebar_and_content">
         <Sidebar />
-        <div className="main_content">컨텐츠</div>
+        <div className="main_content">
+          <div className="archive_container">
+            <div className="archive_search">
+              <div className="archive_search_calendar">달력 검색~</div>
+              <button className="archive_search_btn">검색</button>
+            </div>
+            <div className="archive_content">
+              <button className="archive_btn_left">⬅️</button>
+              <div className="archive_page_left">왼쪽 페이지</div>
+              <div className="archive_page_right">오른쪽 페이지</div>
+              <button className="archive_btn_right">➡️</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

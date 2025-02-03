3539a4b9f4a3195c8486e335967012dad2173dd7
JohnJung-1017 <jungih1017@gmail.com>
2025-02-03 16:14:17 +0900

    design: signIn, signUp

--- frontend/src/components/PopUp.jsx
+++ frontend/src/components/PopUp.jsx
@@ -1,5 +1,6 @@
 import React, { Children } from "react";
 import "../css/PopUp.css";
+import { Icon } from "@iconify/react";
 
 const PopUp = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
@@ -8,7 +9,12 @@ const PopUp = ({ isOpen, onClose, children }) => {
     <div className="popup_overlay" onClick={onClose}>
       <div className="popup" onClick={(e) => e.stopPropagation()}>
         <button className="popup_close_btn" onClick={onClose}>
-          Close
+          <Icon
+            icon="si:close-circle-fill"
+            width="28px"
+            height="28px"
+            style={{ color: "#ffd438" }}
+          />
         </button>
         {children}
       </div>

5730ebe7d0c1b3f5afcc0794f0181cdbfe15a352
JohnJung-1017 <jungih1017@gmail.com>
2025-02-05 13:16:04 +0900

    design: banner
    banner - bottom border radius & logo

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import logoYellow from "../images/Logo_Yellow.png";
 import "../css/Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner_container">
-      <div className="banner_logo">Logo</div>
+      <div className="banner_logo">
+        <img src={logoYellow} alt="Login Page Logo" />
+      </div>
       <div className="banner_btn">
         <div className="banner_username">반갑습니다 일형님!</div>
         <button className="banner_logout">Logout</button>
